bb30ca1654b91ba41c657f76dbf50e1c8a2ad07e
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-02-19 01:38:11 +0530

    feat: added validation to request models

--- src/main/kotlin/com/example/plugins/RequestValidation.kt
+++ src/main/kotlin/com/example/plugins/RequestValidation.kt
@@ -2,7 +2,10 @@ package com.example.plugins
 
 import com.example.data.UserLoginRequest
 import com.example.data.UserSignUpRequest
+import com.example.data.dao.blog.blogDao
 import com.example.data.dao.user.userDao
+import com.example.route.BlogRequest
+import com.example.route.BlogUpdateRequest
 import io.ktor.server.application.*
 import io.ktor.server.plugins.requestvalidation.*
 
@@ -15,18 +18,37 @@ fun Application.configureRequestValidation(){
                 else -> ValidationResult.Valid
             }
         }
-        validate<UserSignUpRequest> { userrequest ->
+        validate<UserSignUpRequest> { usersignuprequest ->
             when {
-                userrequest.username .isBlank() -> ValidationResult.Invalid("Username cannot be null")
-                userrequest.password.isBlank() -> ValidationResult.Invalid("password cannot be null")
+                usersignuprequest.username .isBlank() -> ValidationResult.Invalid("Username cannot be null")
+                usersignuprequest.password.isBlank() -> ValidationResult.Invalid("password cannot be null")
                 else -> when {
-                    userDao.getUser(username = userrequest.username)!=null -> {
+                    userDao.getUser(username = usersignuprequest.username)!=null -> {
                         ValidationResult.Invalid("Unable to signup due to username already reserved")
                     }
                     else -> {
                         ValidationResult.Valid
                     }
                 }
+            }
+        }
+        validate<BlogRequest>{
+            blogRequest ->
+            when{
+                blogRequest.title.isBlank()->ValidationResult.Invalid("Title cannot be null")
+                blogRequest.description.isBlank()->ValidationResult.Invalid("description cannot be null")
+                else->ValidationResult.Valid
+
+            }
+        }
+
+        validate<BlogUpdateRequest>{
+            blogUpdateRequest ->
+            when{
+                blogUpdateRequest.title.isBlank()->ValidationResult.Invalid("title cannot be null")
+                blogUpdateRequest.blogId<0->ValidationResult.Invalid("Blog id cannot be less than 0")
+                blogUpdateRequest.description.isBlank()->ValidationResult.Invalid("description cannot be null")
+                else->ValidationResult.Valid
             }
         }
     }
