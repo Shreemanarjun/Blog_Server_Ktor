059efa63a512d3c902f94f3df1d8e9786c478de7
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-05-18 22:23:10 +0530

    upgrade ktor version,kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ val exposed_version: String by project
 val h2_version: String by project
 val swagger_ui_version:String by project
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("io.ktor.plugin") version "2.2.3"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.8.20"
+    id("io.ktor.plugin") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
 }
 
 group = "com.example"
